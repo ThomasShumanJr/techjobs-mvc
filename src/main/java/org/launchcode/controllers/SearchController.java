1620c1db341f03d70e96c7a07fc17edbf33b95fe
ThomasShumanJr <trsxc5@gmail.com>
2018-05-25 22:56:12 -0500

    Assignment 2 base work done, might go to bonus missions if time allows.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -22,6 +22,19 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<HashMap<String, String>> jobs = new ArrayList();
+        if (searchType.equals("all")) {
+            jobs = JobData.findByValue(searchTerm);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("results", jobs.size() + " Results");
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
 
 }
